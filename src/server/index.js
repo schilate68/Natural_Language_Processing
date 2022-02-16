c2853e65ea59cc029c985632142ca46be4cda982
Juan <lrccfj@outlook.com>
2022-02-16 23:01:19 +0000

    Added Read me text and need to setup the post requests

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,34 @@
 var path = require('path')
 const express = require('express')
+const dotenv = require('dotenv')
 const mockAPIResponse = require('./mockAPI.js')
 
+dotenv.config();
 const app = express()
+
+console.log(process.env.API_KEY);
+
+//TODO: setup POST request for API (use nodefetch or axios)
+/* const formdata = new FormData();
+formdata.append("key", process.env.API_KEY);
+formdata.append("txt", "The football match goals");
+formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+formdata.append("tt", "a");                     // all topics
+
+const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+};
+
+const response = fetch(`https://api.meaningcloud.com/topics-2.0`, requestOptions)
+    .then(response => ({
+        status: response.status,
+        body: response.json()
+    }))
+    .then(({ status, body }) => console.log(status, body))
+    .catch(error => console.log('error', error)); */
+
 
 app.use(express.static('dist'))
 
