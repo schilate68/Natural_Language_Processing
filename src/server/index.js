dc96d5d216ce7310d5e4060e3a6b56730b86b2f5
Juan <lrccfj@outlook.com>
2022-02-17 11:45:26 +0000

    Added Axios post request

--- src/server/index.js
+++ src/server/index.js
@@ -2,33 +2,11 @@ var path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const mockAPIResponse = require('./mockAPI.js')
-
+const axios = require('axios')
 dotenv.config();
 const app = express()
 
 console.log(process.env.API_KEY);
-
-//TODO: setup POST request for API (use nodefetch or axios)
-/* const formdata = new FormData();
-formdata.append("key", process.env.API_KEY);
-formdata.append("txt", "The football match goals");
-formdata.append("lang", "en");  // 2-letter code, like en es fr ...
-formdata.append("tt", "a");                     // all topics
-
-const requestOptions = {
-    method: 'POST',
-    body: formdata,
-    redirect: 'follow'
-};
-
-const response = fetch(`https://api.meaningcloud.com/topics-2.0`, requestOptions)
-    .then(response => ({
-        status: response.status,
-        body: response.json()
-    }))
-    .then(({ status, body }) => console.log(status, body))
-    .catch(error => console.log('error', error)); */
-
 
 app.use(express.static('dist'))
 
@@ -47,3 +25,21 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
+//POST requests
+
+axios
+    .post('https://api.meaningcloud.com/topics-2.0', {
+        key: process.env.API_KEY,
+        txt: "The football match goals",
+        lang: "en",
+        tt: "a"
+    })
+    .then(res => {
+        console.log(`statusCode: ${res.status}`)
+        console.log(res.data)
+    })
+    .catch(error => {
+        console.error(error)
+    })
